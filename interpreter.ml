d8973d6ebf632cb0449245748ceb8d09c214205b
Oualid_CHABANE <oualid.chabane@universite-paris-saclay.fr>
2025-01-05 22:46:23 +0100

    a

--- interpreter.ml
+++ interpreter.ml
@@ -29,14 +29,8 @@ let exec_prog (p: program): unit =
     List.iter2 (fun (par, _) v  -> Hashtbl.add lenv par v) meth.params args;
     (*ajout des vars locals dans *)
     List.iter (fun (par, _) -> Hashtbl.add lenv par Null) meth.locals;
-
     exec_seq meth.code lenv;
 
-    (*(if (f="constructor") then 
-        (* on garde que les attributs avec leurs valeurs initlisés*)
-        Hashtbl.iter (fun s v -> 
-          if(Hashtbl.mem this.fields s) then Hashtbl.replace this.fields s v
-          ) lenv)*)
 
   and exec_seq s lenv =
     (let rec evali e = match eval e with
@@ -52,73 +46,6 @@ let exec_prog (p: program): unit =
     and eval (e: expr): value = match e with
       | Int n  -> VInt n
       | Bool b -> VBool b
-      | Unop (Opp, e) -> 
-        (match eval e with 
-          VInt v -> VInt(-v)
-          | _ -> failwith "Typechecker problem"
-        )
-      | Unop (Not, e) ->         
-        (match eval e with 
-          VBool v -> VBool(not v)
-          | _ -> failwith "Typechecker problem"
-        )
-      | Binop (Add, e1, e2) -> 
-        (match eval e1, eval e2 with 
-          VInt v1, VInt v2 -> VInt(v1 + v2)
-          | _ -> failwith "Typechecker problem"
-        )
-      | Binop (Sub, e1, e2) -> 
-        (match eval e1, eval e2 with 
-          VInt v1, VInt v2 -> VInt(v1 - v2)
-          | _ -> failwith "Typechecker problem"
-        )
-      | Binop (Mul, e1, e2) -> 
-        (match eval e1, eval e2 with 
-          VInt v1, VInt v2 -> VInt(v1 * v2)
-          | _ -> failwith "Typechecker problem"
-        )
-      | Binop (Div, e1, e2) -> 
-        (match eval e1, eval e2 with 
-          VInt v1, VInt v2 -> VInt(v1 / v2)
-          | _ -> failwith "Typechecker problem"
-        )
-      | Binop (Rem, e1, e2) -> 
-        (match eval e1, eval e2 with 
-          VInt v1, VInt v2 -> VInt(v1 mod v2)
-          | _ -> failwith "Typechecker problem"
-        )
-      | Binop (Eq, e1, e2) -> VBool (eval e1 = eval e2)  
-      | Binop (Neq, e1, e2) -> VBool (eval e1 <> eval e2)  
-      | Binop(Lt, e1, e2) -> 
-        (match eval e1, eval e2 with 
-        VInt v1, VInt v2 -> VBool(v1 < v2)
-        | _ -> failwith "Typechecker problem"
-        )
-      | Binop(Le, e1, e2) -> 
-        (match eval e1, eval e2 with 
-        VInt v1, VInt v2 -> VBool(v1 <= v2)
-        | _ -> failwith "Typechecker problem"
-        )
-      | Binop(Gt, e1, e2) ->
-        (match eval e1, eval e2 with 
-        VInt v1, VInt v2 -> VBool(v1 > v2)
-        | _ -> failwith "Typechecker problem"
-        ) 
-      | Binop(Ge, e1, e2) ->
-        (match eval e1, eval e2 with 
-        VInt v1, VInt v2 -> VBool(v1 >= v2)
-        | _ -> failwith "Typechecker problem"
-        )
-      | Binop(And, e1, e2) -> 
-        (match eval e1, eval e2 with 
-        VBool v1, VBool v2 -> VBool(v1 && v2)
-        | _ -> failwith "Typechecker problem"
-        )
-      | Binop(Or, e1, e2) -> 
-        (match eval e1, eval e2 with 
-        VBool v1, VBool v2 -> VBool(v1 || v2)
-        | _ -> failwith "Typechecker problem"
-        )
       | Get (mem_acc) -> 
         (
           match mem_acc with 
@@ -148,11 +75,12 @@ let exec_prog (p: program): unit =
         
       | MethCall (e, s, el) -> 
         (try 
-          eval_call s (evalo e) (List.map (fun e -> eval e) el);
+          eval_call s (evalo e)(List.map (fun e -> eval e) el);
           Null
         with 
           |Return v -> v)
-    
+        
+        | _ -> failwith "zebbi"
     in
   
     let rec exec (i: instr): unit = match i with
